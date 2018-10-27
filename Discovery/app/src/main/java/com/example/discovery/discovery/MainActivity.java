aa2266dbfd0af8eeabd409748b2541e90f018021
Sahil Jamil <Sahil@Sahils-MacBook-Pro.local>
2018-10-27 23:18:12 +0100

    Trying helplessly to add an image how hard can it be

--- Discovery/app/src/main/java/com/example/discovery/discovery/MainActivity.java
+++ Discovery/app/src/main/java/com/example/discovery/discovery/MainActivity.java
@@ -3,10 +3,14 @@ package com.example.discovery.discovery;
 import android.app.DownloadManager;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 
@@ -25,10 +29,20 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        /*try {
+            showImage();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("IO EXception!!");
+        }*/
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -51,6 +65,14 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
+
+    }
+
+    private void showImage() throws MalformedURLException, IOException {
+        URL url = new URL("https://cdn.teslarati.com/wp-content/uploads/2017/11/Roadster_Front_34_1-e1512593855725.jpg");
+        Bitmap bmp = BitmapFactory.decodeStream(url.openConnection().getInputStream());
+        ImageView i = findViewById(R.id.image);
+        i.setImageBitmap(bmp);
 
     }
 }
