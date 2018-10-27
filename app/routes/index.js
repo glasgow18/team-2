41e2186f2c2fb6b0fa71aa017a07b195de4f88b5
Reece <rmctiernan1@gmail.com>
2018-10-28 00:10:12 +0100

    Add route for adding to database

--- app/routes/index.js
+++ app/routes/index.js
@@ -31,4 +31,27 @@ router.get('/activities', function(req, res, next) {
   
 });
 
+router.post('/activities/add/:name/:price/:address/:lat/:lng/:phone?/:email?/:website?/:extra?', function(req, res, next) { 
+  res.send(req.params);
+  if (req.params.phone == undefined) {
+    req.params.phone = null;
+  }
+  if (req.params.email == undefined) {
+    req.params.email = null;
+  }
+  if (req.params.website == undefined) {
+    req.params.website = null;
+  }
+  if (req.params.extra == undefined) {
+    req.params.extra = null;
+  }
+  db.run(`INSERT INTO activity (name, price, address, lat, 
+        lng, contact_phone_number, contact_email, 
+        contact_website, extra_info) VALUES (
+          ${req.params.name}, ${req.params.price}, ${req.params.address}, 
+          ${req.params.lat}, ${req.params.lng}, ${req.params.phone}, ${req.params.email}, ${req.params.website}, ${req.params.extra}
+          )`);
+  console.log(req.params);
+});
+
 module.exports = router;
