6fa2a0cee0b8936f6f265a27e418027062c65333
Sahil Jamil <Sahil@Sahils-MacBook-Pro.local>
2018-10-27 22:38:42 +0100

    add scrolling list display

--- Discovery/app/src/main/java/com/example/discovery/discovery/SearchActivity.java
+++ Discovery/app/src/main/java/com/example/discovery/discovery/SearchActivity.java
@@ -3,9 +3,12 @@ package com.example.discovery.discovery;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -13,24 +16,39 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.gms.maps.MapView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SearchActivity extends AppCompatActivity {
+    List<ActivityPreview> activityPreviews = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
-
-        ActivityPreview test1 = new ActivityPreview("test", "a", (float) 20.5, "123", "123");
+        ActivityPreview test5 = new ActivityPreview("testjkndsa", "19 adfj, adhfjkl aa", (float) 13.23, "123", "123");
+        ActivityPreview test4 = new ActivityPreview("tesjkdt", "a", (float) 53.5, "123", "123");
+        ActivityPreview test1 = new ActivityPreview("test", "a", (float) 46.5, "123", "123");
+        ActivityPreview test2 = new ActivityPreview("test2", "a", (float) 435, "123", "123");
+        ActivityPreview test3 = new ActivityPreview("test3", "a", (float) 345, "123", "123");
+        activityPreviews.add(test1);
+        activityPreviews.add(test2);
+        activityPreviews.add(test3);
+        activityPreviews.add(test4);
+        activityPreviews.add(test5);
         ListView activityList = findViewById(R.id.activityListView);
-        ArrayAdapter<ActivityPreview> activityPreviewArrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,test1);
-        activityList.setAdapter(activityPreviewArrayAdapter);
+
+        ActivityAdapter activityAdapter = new ActivityAdapter();
+        activityList.setAdapter(activityAdapter);
 
         activityList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 //Start a new intent and throw to new activity
+
             }
         });
 
@@ -55,4 +73,38 @@ public class SearchActivity extends AppCompatActivity {
         request.add(stringR);
 
     }
+
+    public class ActivityAdapter extends BaseAdapter {
+        @Override
+        public int getCount() {
+            return activityPreviews.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view = getLayoutInflater().inflate(R.layout.activity_preview_row, null);
+            TextView activityName = view.findViewById(R.id.activityName);
+            TextView address = view.findViewById(R.id.address);
+            TextView price = view.findViewById(R.id.price);
+            MapView mapView = view.findViewById(R.id.mapView);
+
+            activityName.setText(activityPreviews.get(position).getActivityName());
+            address.setText(activityPreviews.get(position).getAddress());
+            price.setText(Float.toString(activityPreviews.get(position).getPrice()));
+
+            return view;
+
+        }
+    }
+
 }
