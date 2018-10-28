11845bd9d51a6e148fc07f37657aab0800a1edb7
Sahil Jamil <Sahil@Sahils-MacBook-Pro.local>
2018-10-28 01:39:15 +0100

    Code for Food

--- app/database/populateTables.sql
+++ app/database/populateTables.sql
@@ -1,36 +1,36 @@
-INSERT INTO activity (name, price, address lat, lng, contact_phone_number, contact_email)
+INSERT INTO activity (name, price, address, lat, lng, contact_phone_number, contact_email)
 VALUES("Multi purpose church hall", 0, "23 Broomhill Rd, Penicuik EH26 9EE", 
 		55.824911, -3.227965, "01968 768954", "phillg13@toucansurf.com");
 
-INSERT INTO activity (name, price, address lat, lng, contact_phone_number)
+INSERT INTO activity (name, price, address, lat, lng, contact_phone_number)
 VALUES("Outdoor Environmental Day Service", 0, "55 Torphin Rd, Edinburgh EH13 0PQ", 
 		55.899668, -3.272208,"0131 441 2055");
 		
-INSERT INTO activity (name, price, address lat, lng, contact_phone_number)
+INSERT INTO activity (name, price, address, lat, lng, contact_phone_number)
 VALUES("Long fence to make sound on at Vogrie", 0, "Edinburgh, city of Edinburgh EH23 4NU", 
 		55.857549, -2.992875, "01875 821716");
 
-INSERT INTO activity (name, price, address lat, lng, contact_website)
+INSERT INTO activity (name, price, address, lat, lng, contact_website)
 VALUES("Wheelchair friendly Cycle Path with 500m long tunnel", 0, "Innocent Railway, Edinburgh EH16 5BQ", 
 		55.941456, -3.172207, "https://www.sustrans.org.uk/ncn/map/route/edinburgh-to-musselburgh");
 
-INSERT INTO activity (name, price, address lat, lng, contact_website)
+INSERT INTO activity (name, price, address, lat, lng, contact_website)
 VALUES("135m Bendy Tunnel with darkness", 0, "Edinburgh EH13 0JX", 
 		55.907921, -3.260044, "http://www.waterofleith.org.uk/walkway/");
 
-INSERT INTO activity (name, price, address lat, lng)
+INSERT INTO activity (name, price, address, lat, lng)
 VALUES("Machines and Steam Trains", 3.50, "Lady Victoria Colliery, Newtongrange, Dalkeith EH22 4QN", 
 		55.862409, -3.066688);
 
-INSERT INTO activity (name, price, address lat, lng, contact_phone_number,contact_email)
+INSERT INTO activity (name, price, address, lat, lng, contact_phone_number,contact_email)
 VALUES("Aquarium", 0, "Dobbies Garden Centre, Melville Nurseries, Lasswade EH18 1AZ", 
 		55.895718, -3.098972,"0131 663 1941","https://www.dobbies.com/find-a-garden-centre/edinburgh/");
 
-INSERT INTO activity (name, price, address lat, lng, contact_phone_number,contact_email)
+INSERT INTO activity (name, price, address, lat, lng, contact_phone_number,contact_email)
 VALUES("Natural tree dens", 0, "Edinburgh, city of Edinburgh EH23 4NU", 
 		55.858177, -2.992503,"01875 821716","https://www.midlothian.gov.uk/info/200283/parks/236/vogrie_country_park");
 
-INSERT INTO activity (name, price, address lat, lng, contact_phone_number,contact_email)
+INSERT INTO activity (name, price, address, lat, lng, contact_phone_number,contact_email)
 VALUES("Natural tree dens", 0, "Edinburgh, city of Edinburgh EH23 4NU", 
 		55.858177, -2.992503,"01875 821716","https://www.midlothian.gov.uk/info/200283/parks/236/vogrie_country_park");
 
--- app/routes/index.js
+++ app/routes/index.js
@@ -31,4 +31,42 @@ router.get('/activities', function(req, res, next) {
   
 });
 
+router.post('/activities/add/:name/:price/:address/:lat/:lng/:phone?/:email?/:website?/:extra?', function(req, res, next) { 
+  res.send(req.params);
+  //Make undefined parametres null to insert into the db
+  if (req.params.phone == undefined) {
+    req.params.phone = null;
+  }
+  if (req.params.email == undefined) {
+    req.params.email = null;
+  }
+  if (req.params.website == undefined) {
+    req.params.website = null;
+  }
+  if (req.params.extra == undefined) {
+    req.params.extra = null;
+  }
+  db.run(`INSERT INTO activity (name, price, address, lat, 
+        lng, contact_phone_number, contact_email, 
+        contact_website, extra_info) VALUES (
+          ${req.params.name}, ${req.params.price}, ${req.params.address}, 
+          ${req.params.lat}, ${req.params.lng}, ${req.params.phone}, ${req.params.email}, ${req.params.website}, ${req.params.extra}
+          )`);
+  console.log(req.params);
+});
+
+router.post('/keywords/add/:keywords', function(req, res, next) { 
+  var splits = req.params.keywords.split(",");
+  console.log(splits);
+  for (var i = 0; i < splits.length; i++) {
+    db.run(`INSERT INTO keyword (word) VALUES ("${splits[i].trim()}")`, function(err) {
+      //We can just ignore duplicate keywords
+      console.log(err);
+    })
+    };
+  
+  res.send(req.param);
+
+});
+
 module.exports = router;
