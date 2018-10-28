943c712a42f05218ece5fedb4634443a36dce51e
Reece <rmctiernan1@gmail.com>
2018-10-28 01:54:23 +0100

    Fix correct tables being added to on new activity

--- app/routes/index.js
+++ app/routes/index.js
@@ -31,8 +31,7 @@ router.get('/activities', function(req, res, next) {
   
 });
 
-router.post('/activities/add/:name/:price/:address/:lat/:lng/:phone?/:email?/:website?/:extra?', function(req, res, next) { 
-  res.send(req.params);
+router.post('/activities/add/:name/:price/:address/:lat/:lng/:phone?/:email?/:website?/:extra?/:keywords', function(req, res, next) { 
   //Make undefined parametres null to insert into the db
   if (req.params.phone == undefined) {
     req.params.phone = null;
@@ -46,26 +45,57 @@ router.post('/activities/add/:name/:price/:address/:lat/:lng/:phone?/:email?/:we
   if (req.params.extra == undefined) {
     req.params.extra = null;
   }
+
+
   db.run(`INSERT INTO activity (name, price, address, lat, 
         lng, contact_phone_number, contact_email, 
         contact_website, extra_info) VALUES (
-          ${req.params.name}, ${req.params.price}, ${req.params.address}, 
-          ${req.params.lat}, ${req.params.lng}, ${req.params.phone}, ${req.params.email}, ${req.params.website}, ${req.params.extra}
+          "${req.params.name}", ${req.params.price}, "${req.params.address}", 
+          ${req.params.lat}, ${req.params.lng}, "${req.params.phone}", "${req.params.email}", "${req.params.website}", "${req.params.extra}"
           )`);
-  console.log(req.params);
-});
 
-router.post('/keywords/add/:keywords', function(req, res, next) { 
-  var splits = req.params.keywords.split(",");
-  console.log(splits);
-  for (var i = 0; i < splits.length; i++) {
+  var activity_id = null;
+  db.get(`SELECT * FROM activity WHERE name = "${req.params.name}"`, function(err, row) {
+    if (err) {
+      console.log(err);
+    }
+      console.log(row);
+      activity_id = row.id;
+
+      var splits = req.params.keywords.split(",");
+  for (var i = 0; i < splits.length - 1; i++) {
+  
+    //trim each string since there can be whitespace from the route url
     db.run(`INSERT INTO keyword (word) VALUES ("${splits[i].trim()}")`, function(err) {
       //We can just ignore duplicate keywords
-      console.log(err);
+      if (err) {
+        console.log(err);
+      }
+
+      if (splits[i] == undefined) {
+        console.log("HIT UNDEFINED at " + i);
+      }
+    db.get(`SELECT * FROM keyword WHERE word = "${splits[i].trim()}"`, function(err, row) {
+      if (err) {
+        console.log(err);
+        console.log(err);
+            console.log(splits[i]);
+            console.log(i);
+      }
+      console.log(splits[0]);
+        db.run(`INSERT INTO activity_to_keyword VALUES (${activity_id}, ${row.id})`, function(err) {
+          if (err) {
+            
+          }
+        });
+    });
     })
+
     };
+  });
   
-  res.send(req.param);
+  
+  res.send(req.params);
 
 });
 
